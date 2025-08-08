faca3aec9ca8f95bd3cc14ce6fd0247bea29c5c3
julianhaddadj <julianhaddadj@gmail.com>
2025-08-08 22:34:16 +0400

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,9 +7,9 @@ const mongoose = require('mongoose');
  */
 const connectDB = async () => {
   const mongoUri = process.env.MONGODB_URI;
-  if (!mongoUri) {
-    throw new Error('MONGO_URI is not defined in environment variables');
-  }
+if (!mongoUri) {
+  throw new Error('MONGODB_URI is not defined in environment variables');
+}
   try {
     await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
